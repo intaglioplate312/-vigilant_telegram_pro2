5fdc563546f73755dfdb60e609f151d779cb3331
intaglioplate312 <helenelizabeth@me.com>
2017-08-21 13:47:01 -0500

    inital structure

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+var port = 3000;
+app.listen(port);
+
+app.get('/', function(require, result) {
+    result.render('index');
+});
